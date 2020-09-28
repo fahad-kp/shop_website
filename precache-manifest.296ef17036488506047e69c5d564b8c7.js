09ac86447f0741976f83d62f5dd5b28c9889a98f
fahad-kp <fahadkp999@gmail.com>
2020-09-28 22:59:22 +0530

    Updates

---
+++ precache-manifest.296ef17036488506047e69c5d564b8c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b973d8e16d46596bb5854aa1bd372881",
+    "url": "/shop_website/index.html"
+  },
+  {
+    "revision": "75856dbd1dbe78a68f86",
+    "url": "/shop_website/static/css/main.1ad4affb.chunk.css"
+  },
+  {
+    "revision": "d8e959dd2051d17933d5",
+    "url": "/shop_website/static/js/2.6cf41661.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shop_website/static/js/2.6cf41661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75856dbd1dbe78a68f86",
+    "url": "/shop_website/static/js/main.0969d851.chunk.js"
+  },
+  {
+    "revision": "a979e3700e26b725345e",
+    "url": "/shop_website/static/js/runtime-main.7838c9f4.js"
+  },
+  {
+    "revision": "ab3944394e9dcbac0b6fa772b1cf8b94",
+    "url": "/shop_website/static/media/image1.ab394439.png"
+  },
+  {
+    "revision": "e2bac83c51bb0a225ae0e30f4aa423b7",
+    "url": "/shop_website/static/media/image2.e2bac83c.png"
+  },
+  {
+    "revision": "f5741837362dc36fdfa0f9dde48fe6da",
+    "url": "/shop_website/static/media/image3.f5741837.png"
+  },
+  {
+    "revision": "f98a194f6c2ca49930158b23861c001c",
+    "url": "/shop_website/static/media/image4.f98a194f.png"
+  }
+]);
